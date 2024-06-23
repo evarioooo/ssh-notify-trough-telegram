4618d682ec3c58490da65deb736e3a86f0ad8617
Alexander Bergmann <152819279+evarioooo@users.noreply.github.com>
2024-06-23 14:33:05 +0200

    telegram-notify.sh aktualisieren

--- telegram-notify.sh
+++ telegram-notify.sh
@@ -3,13 +3,13 @@
 # This file determines the content of your message.
 # If you know what you are doing, you can extend 
 # the script with other parameters.
-# For example, it would be conceivable to query the respective IP with an API in order to obtain more data.
+# For example, it would be conceivable to query
+# the respective IP with an API in order to obtain more data.
     
 # prepare any message you want
 login_ip="$(echo $SSH_CONNECTION | cut -d " " -f 1)"
 login_date="$(date +"%e %b %Y, %a %r")"
 login_name="$(whoami)"
-host="$(hostname -s)"
 
 # For new line I use $'\n' here
 message="New login to server"$'\n'"User: $login_name"$'\n'"Ip: $login_ip"$'\n'"Date: $login_date"
